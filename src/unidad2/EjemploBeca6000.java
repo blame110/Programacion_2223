eba6df9da29bfc717a6d021b0f932b108da27eb7
blame110 <46011863+blame110@users.noreply.github.com>
2022-10-11 14:04:11 +0200

    actualizacion

---
+++ src/unidad2/EjemploBeca6000.java
@@ -0,0 +1,19 @@
+package unidad2;
+
+public class EjemploBeca6000 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Mayor de 16 años
+		 * renta entre 15000 30000 euros anuales
+		 * sin suspensos
+		 * sin trabajo
+		 * 
+		 */
+		
+		
+		
+	}
+
+}
