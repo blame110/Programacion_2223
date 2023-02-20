e4d04719d3bd288329444b414514b151cf02a653
blame110 <46011863+blame110@users.noreply.github.com>
2023-02-20 10:46:08 +0100

    Examen Tema3

---
+++ src/unidad3/PruebaExamen.java
@@ -0,0 +1,14 @@
+package unidad3;
+
+public class PruebaExamen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Pasajero juan = new Pasajero();
+
+		System.out.println(juan);
+
+	}
+
+}
